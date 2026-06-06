08f7b08eae8ba8a989e266dcc3db0efb453d6d81
prak-coder <prakash.change@gmail.com>
2026-06-06 12:45:39 +0530

    get /api/v1/tours endpoint from jsonfile

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server', app: 'natours' });
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello from the server', app: 'natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.send('you can post at this end point..');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
-app.post('/', (req, res) => {
-  res.send('you can post at this end point..');
-});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
