9df7c35c04ff07692e2bbe811017a2d40fd0e2dd
prak-coder <prakash.change@gmail.com>
2026-06-10 10:36:36 +0530

    create patch and delete tour routes

--- app.js
+++ app.js
@@ -68,6 +68,31 @@ app.post('/api/v1/tours', (req, res) => {
     },
   );
 });
+//patch req for update
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: '<Updated tour here..>',
+      },
+    });
+  }
+});
+
+//delete req
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  } else {
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
