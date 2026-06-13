100b4faa132fe8ddfd9338536ef11f6490ee5093
prak-coder <prakash.change@gmail.com>
2026-06-13 12:18:53 +0530

    connect db mongoose schema and model add new tours

--- server.js
+++ server.js
@@ -1,9 +1,67 @@
+const dns = require('node:dns');
+
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+//Many local ISP routers or default network setups cannot parse
+//  or handle complex SRV lookups correctly.
+
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
-
+const { type } = require('node:os');
+const e = require('express');
 const app = require('./app');
-// console.log(process.env);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB sucessfully connected');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour should have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A true should have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log('Error 🧨 ', err);
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
