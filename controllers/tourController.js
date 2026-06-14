1c56dd1f5740370db22eab67c4f77d08139d0028
prak-coder <prakash.change@gmail.com>
2026-06-14 16:12:16 +0530

    create tour mvc in atlas

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -24,13 +24,26 @@ exports.getTour = (req, res) => {
 };
 
 //post req same url only method -post rest api rule
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+   try {
+    const newTour = await Tour.create(req.body);
   res.status(201).json({
     status: 'success',
     data: {
-      // tour: newTour,
+      tour: newTour,
     },
   });
+   } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status:'fail',
+      message:error
+    })
+    
+    
+   }
+
+  
 };
 
 exports.updateTour = (req, res) => {
