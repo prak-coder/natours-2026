5d6736b5fcf74763976f8d6656b82df43d84cb0c
prak-coder <prakash.change@gmail.com>
2026-06-15 12:43:17 +0530

    delete a tour by id in atlas

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,13 +53,13 @@ exports.updateTour = async (req, res) => {
   try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators:true
+      runValidators: true,
     });
     res.status(200).json({
       status: 'success',
       message: 'tour updated',
       data: {
-         tour,
+        tour,
       },
     });
   } catch (error) {
@@ -70,12 +70,18 @@ exports.updateTour = async (req, res) => {
     });
   }
 };
-exports.deleteTour = (req, res) => {
-  // if (req.params.id * 1 > tours.length) {
-  //   return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  // }
-  // res.status(204).json({
-  //   status: 'success',
-  //   data: null,
-  // });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data:null
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
