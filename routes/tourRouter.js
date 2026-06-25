08d33714005463609305647f98a0ed521ccddd7f
prak-coder <prakash.change@gmail.com>
2026-06-25 10:34:25 +0530

    find how many tours per month using aggregate pipeline unwind and project

--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -7,6 +7,8 @@ const tourController = require('../controllers/tourController');
 // router.param('id', tourController.checkId);
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/tour-monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
