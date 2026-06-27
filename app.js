e4f97194683bf460fd054a2207841af8b4bc2e3d
prak-coder <prakash.change@gmail.com>
2026-06-27 10:05:03 +0530

    error-handling: appErrorclass errorcontroller

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
@@ -44,25 +46,10 @@ app.use('/api/v1/users', userRouter);
 
 //routes that doesnot match any of previous routes for all methods get put patch delete
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `cant find the ${req.originalUrl} on this server`,
-  // });
-  const err = new Error(`cant find the ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  next(err);
+  next(new AppError(`cant find the ${req.originalUrl} on this server`, 404));
 });
 
 //Global Error handling middleware
-app.use((err, req, res, next) => {
-  err.status = err.status || 'error';
-  err.statusCode = err.statusCode || 500;
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-  next();
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
