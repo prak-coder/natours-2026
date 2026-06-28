01715264e9e39440ed358d959c17099d05ff9713
prak-coder <prakash.change@gmail.com>
2026-06-28 11:37:58 +0530

    err-handling:unhandled rejection

--- server.js
+++ server.js
@@ -31,6 +31,14 @@ mongoose
   });
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDELED REJECTION.🧨 Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
 });
