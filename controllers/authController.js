83bc470e8cd8a304fcb9929cc6eab63d3b71c481
prak-coder <prakash.change@gmail.com>
2026-07-01 12:24:41 +0530

    authetication: login user route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,40 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
+const AppError = require('../utils/appError');
+
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
   res.status(201).json({
     status: 'success',
+    token,
     data: newUser,
   });
   next();
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  //1.check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('please provide email and password', 400));
+  }
+  //2.check if user exist and password correct
+
+  //3.if everything ok send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+};
