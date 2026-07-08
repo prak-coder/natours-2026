ff08697ffdeb94efd3235c5c4bd8be4629d30fb4
prak-coder <prakash.change@gmail.com>
2026-07-08 11:41:00 +0530

    security:set http headers with helmet

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const morgan = require('morgan');
 
 const rateLimit = require('express-rate-limit');
 
+const helmet = require('helmet');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRouter');
 const userRouter = require('./routes/userRouter');
 
 //1)Global MIDDLEWARE
-
+//set security headers
+app.use(helmet());
+//to set dev
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+
+//body parser middle ware
+//limit body to 10kb
+app.use(express.json({ limit: '10kb' }));
 
 //middle to serve static files
 app.use(express.static(`${__dirname}/public`));
