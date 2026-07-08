3e9429b0f601bc732f8eed120e46cd7ed8bbd3a5
prak-coder <prakash.change@gmail.com>
2026-07-08 12:19:27 +0530

    security:prevent parameter pollution

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 
 const app = express();
-
 const morgan = require('morgan');
-
 const rateLimit = require('express-rate-limit');
-
 const helmet = require('helmet');
-
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -32,6 +29,19 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 ///Data sanitization against XSS
 app.use(xss());
+//parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 //middle to serve static files
 app.use(express.static(`${__dirname}/public`));
 
