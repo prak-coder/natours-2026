c4b83d62b74d588557b5f94c582872d99f1e8992
prak-coder <prakash.change@gmail.com>
2026-07-09 17:06:20 +0530

    data-modelling:virtual populate review in get one tour

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,9 +35,6 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, function (next) {
   // eslint-disable-next-line no-unused-expressions
   this.populate({
-    path: 'tour',
-    select: 'name ',
-  }).populate({
     path: 'user',
     select: 'name photo',
   });
--- models/tourModel.js
+++ models/tourModel.js
@@ -121,6 +121,12 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationinweeks').get(function () {
   return this.duration / 7;
 });
+//virtual populate tour with reviews
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
 //document middleware only work for save and create mongodb method not insert/update
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
