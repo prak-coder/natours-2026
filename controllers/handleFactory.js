d8dd54ba71f89f7b1ec116c3bd1c9d3e4e3ce2dc
prak-coder <prakash.change@gmail.com>
2026-07-12 09:10:30 +0530

    refactor:factory func getall all resources

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -6,7 +6,7 @@ exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
     if (!doc) {
-      return next(new AppError('No doc with that id', 404));
+      return next(new AppError('No document with that id', 404));
     }
     res.status(204).json({
       status: 'success',
