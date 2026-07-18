c297cf0079896817457112415339e1c576cd2fcd
prak-coder <prakash.change@gmail.com>
2026-07-18 15:23:37 +0530

    multer storage and filter and store filename to DB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,32 @@
+const multer = require('multer');
 const User = require('../models/userModel');
-
 const catchAsync = require('../utils/catchAsync');
-
 const AppError = require('../utils/appError');
-
 const factory = require('./handleFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image. Please upload only images', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.updateUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -22,9 +44,6 @@ exports.createUser = (req, res) => {
 };
 //update me seperate route bcs used by logged in user to update username and email not password
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
-
   //1.create error if user posted password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
@@ -34,6 +53,9 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   //2.update user document
   //white listing bcs no want user to change role='admin'
   const filterBody = filterObj(req.body, 'name', 'email');
+
+  if (req.file) filterBody.photo = req.file.filename;
+
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
     new: true,
     runValidators: true,
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,9 @@
 const express = require('express');
 
 const router = express.Router();
-const multer = require('multer');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
-const upload = multer({ dest: 'public/img/users' });
 //open to all
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
@@ -17,7 +15,11 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
-router.patch('/updateMe', upload.single('photo'), userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.updateUserPhoto,
+  userController.updateMe,
+);
 router.delete('/deleteMe', userController.deleteMe);
 router.get('/me', userController.getMe, userController.getUser);
 
