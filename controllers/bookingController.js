85c3912c79375c185f328858c13e06c44ccc9768
prak-coder <prakash.change@gmail.com>
2026-07-24 12:37:02 +0530

    stripe payment session and bookings resourse create

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -5,6 +5,7 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handleFactory');
+const { enabled } = require('../app');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //1.get currently booked tour
@@ -13,6 +14,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   if (!tour) next(new AppError(400, 'no such tour'));
   //2.create checkout session
   const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
     payment_method_types: ['card'],
     success_url: `${req.protocol}://${req.get('host')}`,
     cancel_url: `${req.protocol}://${req.get('host')}/`,
@@ -20,17 +22,22 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100, // Stripe expects the amount in cents
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+            ],
+          },
+        },
         quantity: 1,
       },
     ],
   });
+
   //3.create session as a response
   res.status(200).json({
     status: 'success',
